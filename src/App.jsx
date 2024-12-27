c62b05e37a1fa96f03219cee86da5ed46943afb3
Christian Lee <christianlee101@gmail.com>
2024-12-26 21:41:44 -0500

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './Nav.css'
+
+function Nav() {
+
+  return (
+    <>
+      <nav id="navbar">
+        <h1> RoomBoard </h1>
+        <ul class="nav-links">
+          <li> <a class="nav-link"> About </a> </li>
+          <li> <a class="nav-link"> Explore </a> </li>
+          <li> <a class="nav-link"> Get Started </a> </li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </>
+  );
+}
 
 function App() {
 
   return (
     <>
-      <h1> poopy list </h1>
+      <Nav> </Nav>
     </>
   );
 }
