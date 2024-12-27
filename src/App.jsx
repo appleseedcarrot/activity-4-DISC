e30d375e24ccd21e8d8e9919fba5ce79172b8f5e
Christian Lee <christianlee101@gmail.com>
2024-12-26 22:17:48 -0500

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './Nav.css'
+import './Footer.css'
 
 function Nav() {
 
   return (
     <>
       <nav id="navbar">
-        <h1> RoomBoard </h1>
-        <ul class="nav-links">
+        <h2 id="logo"> RoomBoard </h2>
+        <ul id="nav-links">
           <li> <a class="nav-link"> About </a> </li>
           <li> <a class="nav-link"> Explore </a> </li>
           <li> <a class="nav-link"> Get Started </a> </li>
@@ -21,11 +22,36 @@ function Nav() {
   );
 }
 
+function Footer() {
+  return (
+    <div id="footer">
+      <ul>
+        <li> Home </li>
+        <li> About </li>
+        <li> Explore </li>
+        <li> Sign In </li>
+      </ul>
+      <p> Roomboard @ 2024 All rights reserved. </p>
+    </div>
+  )
+}
+
+// function OptionBar() {
+
+//   return (
+//     <>
+
+//     </>
+//   );
+// }
+
 function App() {
 
   return (
     <>
       <Nav> </Nav>
+      <h1> Explore </h1>
+      <Footer> </Footer>
     </>
   );
 }
