00057c5f48295a8ee887c674ffe5827d88b14cc1
Christian Lee <christianlee101@gmail.com>
2024-12-29 19:43:15 -0500

    Now you can use the navbar to go between homepage and explore

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 import './Nav.css'
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
 
     return (
       <>
         <nav id="navbar">
-          <h2 id="logo"> RoomBoard </h2>
+          <Link to="/"> <h2 id="logo"> RoomBoard </h2> </Link> 
           <ul id="nav-links">
             <li> <a class="nav-link"> About </a> </li>
-            <li> <a class="nav-link"> Explore </a> </li>
+            <li> <Link to="/Explore" class="nav-link"> Explore </Link> </li>
             <li> <a class="nav-link"> Get Started </a> </li>
           </ul>
         </nav>
