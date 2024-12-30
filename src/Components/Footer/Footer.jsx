8cb9b976a8dd07627e8097726cdde667bf6f9c74
Christian Lee <christianlee101@gmail.com>
2024-12-29 19:44:35 -0500

    You can now go between home page and explore using footer links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import "./Footer.css"
+import { Link } from "react-router-dom"
 
 export default function Footer() {
     return (
       <div id="footer">
         <ul>
-          <li> <a> Home </a> </li>
+          <li> <Link to="/"> Home </Link> </li>
           <li> <a> About </a> </li>
-          <li> <a> Explore </a> </li>
+          <li> <Link to="/Explore"> Explore </Link> </li>
           <li> <a> Sign In </a> </li>
         </ul>
         <p> Roomboard @ 2024 All rights reserved. </p>
